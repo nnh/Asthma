f22bb1af6b0f07f207b13343687bdb6df95ca853
ma3k0 <mamiko.yonejima@nnh.go.jp>
2017-07-19 14:21:50 +0900

    渡邉さん修正後

--- programs/Asthma_tokyohosp.R
+++ programs/Asthma_tokyohosp.R
@@ -6,24 +6,29 @@
 # 作業ディレクトリの設定
 
 
+#ファイル名の設定
+#ここにcsvファイル名を入力する
+filename <- "Asthma_registration_170627_1154.csv"
+
 # csvファイル読み込み
 setwd("../rawdata")
 getwd()
 
-regi.data <- read.csv("Asthma_registration_170627_1154.csv", na.strings = "", as.is = T)
+regi_data <- read.csv(filename, na.strings = "", as.is = T)
 
-# regi.dataの項目「シート作成時施設名」」と「現担当医」と「症例登録番号」と「通し番号」をDFに入れる
-regi <- regi.data[, c(6,14,18,32)]
-#  names(regi) #  不要かな。
+# regi_dataの項目「シート作成時施設名」」と「現担当医」と「症例登録番号」と「通し番号」をDFに入れる
+regi <- regi_data[, c(6,14,18,32)]
+
 # 項目名「シート作成時施設名」を「施設名」に変更
 names(regi)[1] <- c("施設名")
+
 # 項目名「現担当医」を「担当医名」に変更
 names(regi)[2] <- c("担当医名")
-# names(regi) #  不要かな。
+
 
 # 項目名を並べ替える
 asthmadata <- regi[, c(3, 1, 2, 4)]
-# asthmadata #  不要かな。
+
 
 #NAなら空値にして。書き出し
 asthmadata[is.na(asthmadata)] <- ""
--- programs/Asthma_zenbe.R
+++ programs/Asthma_zenbe.R
@@ -5,38 +5,41 @@
 
 #作業ディレクトリの設定
 
+#ファイル名の設定
+#ここにcsvファイル名を入力する
+FN1 <- "Asthma_registration_170627_1154.csv"
+FN2 <- "Asthma_baseline_slight_170615_0948.csv"
+FN3 <- "Asthma_baseline_heavy_170615_0948.csv"
 
 #csvファイル読み込み
 
 setwd("../rawdata")
 getwd()
 
-regi.data <- read.csv("Asthma_registration_170627_1154.csv", na.strings = "", as.is = T)
-baseline.data <- read.csv("Asthma_baseline_slight_170615_0948.csv", na.strings = "", as.is = T)
-baselineheavy.data<-read.csv("Asthma_baseline_heavy_170615_0948.csv", na.strings = "", as.is = T)
+regi_data <- read.csv(FN1, na.strings = "", as.is = T)
+baseline_data <- read.csv(FN2, na.strings = "", as.is = T)
+baselineheavy_data<-read.csv(FN3, na.strings = "", as.is = T)
 
-#regi.dataの項目「シート作成時施設名」」と「現担当医」と「症例登録番号」と「年齢」と「通し番号」をDFに入れる
-regi <- regi.data[, c(6, 14, 18, 30, 32)]
-# names(regi) #  remove?
+#regi_dataの項目「シート作成時施設名」」と「現担当医」と「症例登録番号」と「年齢」と「通し番号」をDFに入れる
+regi <- regi_data[, c(6, 14, 18, 30, 32)]
+
 #項目名「シート作成時施設名」を「施設名」に変更
 names(regi)[1] <- c("施設名")
+
 #項目名「現担当医」を「担当医名」に変更
 names(regi)[2] <- c("担当医名")
-# names(regi) #  remove?
 
-#baseline.dataの項目「症例登録番号」と「性別」をDFに入れる
-baseline <- baseline.data[, c(18, 28)]
-# names(baseline) #  remove?
+#baseline_dataの項目「症例登録番号」と「性別」をDFに入れる
+baseline <- baseline_data[, c(18, 28)]
+
 #項目名を「性別.1」から「性別_重症でない」に変更
 names(baseline)[2] <- c("性別_重症でない")
-# names(baseline) #  remove?
 
-#baselineheavy.dataの項目「症例登録番号」と「性別」をDFに入れる
-baselineheavy <- baselineheavy.data[, c(18, 28)]
-# names(baselineheavy) #  remove?
+#baselineheavy_dataの項目「症例登録番号」と「性別」をDFに入れる
+baselineheavy <- baselineheavy_data[, c(18, 28)]
+
 #項目名を「性別.1」から「性別_重症」に変更
 names(baselineheavy)[2] <- c("性別_重症")
-# names(baselineheavy) #  remove?
 
 #baselineとbaselineheavyを「症例登録番号」をキーにマージ
 sexdata <- merge(baseline, baselineheavy, by="症例登録番号", all = T)
@@ -51,7 +54,6 @@ merge2$"性別" <- ifelse((merge2$性別_重症でない == "女性" & is.na(mer
 
 
 #項目名を並べ替える
-# names(merge2)#  remove?
 asthmadata <- merge2[, c(1, 2, 3, 5, 8, 4)]
 
 #NAなら空値にして。書き出し
